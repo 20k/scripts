78362b6a4b93b02233f9f3faa78138d3a0598653
20k <icedshot@gmail.com>
2022-12-17 05:56:21 +0000

    update debug script

--- debugimgui.lua
+++ debugimgui.lua
@@ -1,12 +1,20 @@
 local gui = require('gui')
-local imgui = require('imgui')
 
 MyScreen = defclass(MyScreen, gui.Screen)
 
+state = false;
+
 function MyScreen:render()
 	self:renderParent()
-    dfhack.imgui.Begin()
+    dfhack.imgui.Begin("Script Title");
 	dfhack.imgui.Text("Help I'm Trapped In A Script!!")
+	
+	if(dfhack.imgui.Button("Button")) then
+		state = not state
+	end
+	
+	dfhack.imgui.Text("Button State: " .. tostring(state))
+	
 	dfhack.imgui.End();
 end
 
