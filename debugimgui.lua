870c051f15259d05b56af138ec4c934420255464
20k <icedshot@gmail.com>
2022-12-17 05:47:45 +0000

    update debugging

--- debugimgui.lua
+++ debugimgui.lua
@@ -1,10 +1,13 @@
 local gui = require('gui')
+local imgui = require('imgui')
 
 MyScreen = defclass(MyScreen, gui.Screen)
 
 function MyScreen:render()
 	self:renderParent()
-    dfhack.internal.debugImGui()
+    dfhack.imgui.Begin()
+	dfhack.imgui.Text("Help I'm Trapped In A Script!!")
+	dfhack.imgui.End();
 end
 
 screen = MyScreen{ }:show()
