5c403a2a8d1f60789155f03a58f13fe24c5dc14c
20k <icedshot@gmail.com>
2022-12-18 01:26:20 +0000

    add overlay2

---
+++ gui/overlay2.lua
@@ -0,0 +1,98 @@
+-- overlay plugin gui config
+--@ module = true
+
+local gui = require('gui')
+local guidm = require('gui.dwarfmode')
+local widgets = require('gui.widgets')
+
+local overlay = require('plugins.overlay')
+
+filterState = 0
+
+function get_filter_name(state)
+	if(state == 0) then
+		return "overlays for the current screen"
+	end
+	
+	if(state == 1) then
+		return "all overlays"
+	end
+end
+
+OverlayConfig = defclass(OverlayConfig, gui.Screen)
+
+function OverlayConfig:init()
+	self.scr_name = overlay.simplify_viewscreen_name(
+            getmetatable(dfhack.gui.getCurViewscreen(true)))
+end
+
+function OverlayConfig:render()
+	self:renderParent()
+	
+	dfhack.imgui.Begin("Overlay ImGui Config")
+	
+	local state = overlay.get_state()
+	
+	dfhack.imgui.Text("Current screen: " .. self.scr_name)
+	
+	local filter_name = "Showing: " .. get_filter_name(filterState)
+	
+	if(dfhack.imgui.Button(filter_name)) then
+		filterState = (filterState + 1) % 2
+	end
+	
+	local to_set = {}
+	
+	for _,name in ipairs(state.index) do
+		local db_entry = state.db[name]
+        local widget = db_entry.widget
+        if not widget.hotspot and filterState ~= 1 then
+            local matched = false
+            for _,scr in ipairs(overlay.normalize_list(widget.viewscreens)) do
+                if overlay.simplify_viewscreen_name(scr) == self.scr_name then
+                    matched = true
+                    break
+                end
+            end
+            if not matched then goto continue end
+        end
+	
+		local cfg = state.config[name]
+	
+		local col = cfg.enabled and "LIGHTGREEN" or "YELLOW"
+		local txt = cfg.enabled and "enabled" or "disabled"
+		
+		if(dfhack.imgui.Button("[" .. txt .. "]##" .. name)) then
+			cfg.enabled = not cfg.enabled
+			
+			local command = 'disable'
+			
+			if(cfg.enabled) then
+				command = 'enable'
+			end
+			
+			to_set[name] = command;
+		end
+		
+		dfhack.imgui.SameLine()
+		
+		dfhack.imgui.Text(name)
+		
+		::continue::
+	end
+
+	dfhack.imgui.End();
+	
+	for name, command in pairs(to_set) do
+		--dfhack.imgui.Text(name)
+		--dfhack.imgui.Text(command)
+	
+		overlay.overlay_command({command, name}, true)
+	end
+end
+
+if dfhack_flags.module then
+    return
+end
+
+view = view or OverlayConfig{}:show()
