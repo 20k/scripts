bdf8c7ed73876c7703e51be1f74d9ccc55d5cee3
20k <icedshot@gmail.com>
2022-12-28 07:14:58 +0000

    click on buildings

--- dfui_viewitems.lua
+++ dfui_viewitems.lua
@@ -3,6 +3,9 @@
 render = reqscript('dfui_render')
 imgui = dfhack.imgui
 
+selected_building_pos = {x=-1, y=-1, z=-1}
+
+
 function render_viewitems()
 	local top_left = render.get_camera()
 
@@ -11,7 +14,19 @@ function render_viewitems()
 	local lx = top_left.x+mouse_pos.x - 1
 	local ly = top_left.y+mouse_pos.y - 1
 	
-	local building = dfhack.buildings.findAtTile(xyz2pos(lx, ly, top_left.z))
+	local check_x = selected_building_pos.x
+	local check_y = selected_building_pos.y
+	local check_z = selected_building_pos.z
+	
+	local has_item = render.get_menu_item()
+	
+	if not has_item then
+		check_x = lx
+		check_y = ly
+		check_z = top_left.z
+	end
+	
+	local building = dfhack.buildings.findAtTile(xyz2pos(check_x, check_y, check_z))
 	
 	if building ~= nil then	
 		local str = df.new("string")
@@ -38,6 +53,15 @@ function handle_mouseover()
 	local building = dfhack.buildings.findAtTile(xyz2pos(lx, ly, top_left.z))
 	
 	if building ~= nil then	
+		if imgui.IsMouseClicked(0) and not imgui.WantCaptureMouse() then
+			selected_building_pos.x = lx
+			selected_building_pos.y = ly
+			selected_building_pos.z = top_left.z
+			
+			render.push_menu("View Items In Buildings")
+			render.set_menu_item(true)
+		end
+	
 		local str = df.new("string")
 		building:getName(str)
 		
