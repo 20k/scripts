55153817fc25165ceac4ea57f25fac3a0ebe4819
20k <icedshot@gmail.com>
2022-12-28 06:47:55 +0000

    tooltip hover

--- dfui_viewitems.lua
+++ dfui_viewitems.lua
@@ -18,12 +18,31 @@ function render_viewitems()
 		building:getName(str)
 		
 		imgui.Text(str.value)
-		imgui.SetTooltip(str.value)
 		
 		str:delete()
 	end
 	
 	if imgui.Button("Back") or (imgui.IsWindowHovered(0) and imgui.IsMouseClicked(1)) then
 		render.pop_menu()
+	end
+end
+
+function handle_mouseover()
+	local top_left = render.get_camera()
+
+	local mouse_pos = imgui.GetMousePos()
+	
+	local lx = top_left.x+mouse_pos.x - 1
+	local ly = top_left.y+mouse_pos.y - 1
+	
+	local building = dfhack.buildings.findAtTile(xyz2pos(lx, ly, top_left.z))
+	
+	if building ~= nil then	
+		local str = df.new("string")
+		building:getName(str)
+		
+		imgui.SetTooltip(str.value)
+		
+		str:delete()
 	end
 end
