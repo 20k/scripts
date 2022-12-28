b407ca3d6c2a0e0a26794d5d47514612f20c5c7f
20k <icedshot@gmail.com>
2022-12-28 06:33:33 +0000

    viewitems

---
+++ dfui_viewitems.lua
@@ -0,0 +1,25 @@
+--@ module = true
+
+render = reqscript('dfui_render')
+imgui = dfhack.imgui
+
+function render_viewitems()
+	local top_left = render.get_camera()
+
+	local mouse_pos = imgui.GetMousePos()
+	
+	local lx = top_left.x+mouse_pos.x - 1
+	local ly = top_left.y+mouse_pos.y - 1
+	
+	local building = dfhack.buildings.findAtTile(xyz2pos(lx, ly, top_left.z))
+	
+	if building ~= nil then	
+		local str = df.new("string")
+		building:getName(str)
+		
+		imgui.Text(str.value)
+		imgui.SetTooltip(str.value)
+		
+		str:delete()
+	end
+end
