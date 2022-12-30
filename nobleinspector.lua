47f81f62d7478e17410af30d677838ade15cbb0d
20k <icedshot@gmail.com>
2022-12-30 02:48:36 +0000

    first pass dorf

--- nobleinspector.lua
+++ nobleinspector.lua
@@ -5,46 +5,55 @@ imgui = dfhack.imgui
 Inspector = defclass(Inspector, gui.Screen)
 
 function Inspector:init()
-	render.reset_menu_to("main")
+	
 end
 
+function noble_position(unit)
+
+end
 
 function Inspector:render()
 	self:renderParent()
 	
 	if(imgui.IsKeyPressed(6)) then
-		state = "main"
-		--self:dismiss()
+		self:dismiss()
 	end
 	
 	imgui.Begin("Hi")
 	
-	local units = df.global.word.units.active
+	local units = df.global.world.units.active
 	
 	for i=0,#units-1 do
 		local unit = units[i]
 		
-		local race_name = dfhack.units.getRaceName(unit)
+		local race_name = dfhack.df2utf(dfhack.units.getRaceName(unit))
+		
+		if race_name ~= "DWARF" then
+			goto continue
+		end
+		
 		local language_name = dfhack.units.getVisibleName(unit)
 		
-		local first_name = language_name.first_name
+		local first_name = dfhack.df2utf(language_name.first_name)
 		
 		imgui.Text("Race: " .. race_name .. " : Name:" .. first_name) 
+		
+		::continue::
 	end
 	
 	imgui.End()
 	
 end
 
-function MyScreen:onDismiss()
+function Inspector:onDismiss()
 	state = "main"
     view = nil
 end
 
-function MyScreen:onInput(keys)
+function Inspector:onInput(keys)
 	if not imgui.WantCaptureInput()then
 		imgui.FeedUpwards()
 	end
 end
 
-screen = MyScreen{ }:show()
+screen = Inspector{ }:show()
