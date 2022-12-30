e77af855b5521ca74c7d62a84d009acffb108075
20k <icedshot@gmail.com>
2022-12-30 04:03:28 +0000

    noble inspector

--- nobleinspector.lua
+++ nobleinspector.lua
@@ -8,8 +8,61 @@ function Inspector:init()
 	
 end
 
-function noble_position(unit)
+function fnd(array, fieldname, fieldvalue)
+	--for _,v in ipairs(array) do
+	for i=0,#array-1 do
+		if array[i].id == fieldvalue then
+			return array[i]
+		end
+	end
+	
+	return nil
+end
 
+function noble_position(unit)
+	local histfig = df.historical_figure.find(unit.hist_figure_id)
+		
+	if histfig == nil then
+		imgui.Text("No noble")
+		return
+	end
+	
+	local entity_links = histfig.entity_links
+	
+	for i=0,#entity_links-1 do
+		local link = entity_links[i]
+		
+		if not df.is_instance(df.histfig_entity_link_positionst, link) then
+			goto notnoble
+		end
+		
+		local epos = link
+		
+		local entity = df.historical_entity.find(epos.entity_id)
+		
+		if entity == nil then
+			imgui.Text("no noble 2")
+			goto notnoble
+		end
+				
+		local assignment = fnd(entity.positions.assignments, "id", epos.assignment_id)
+		
+		if assignment == nil then
+			imgui.Text("no noble 3")
+			goto notnoble
+		end
+		
+		local position = fnd(entity.positions.own, "id", assignment.position_id)
+		
+		if position == nil then 
+			imgui.Text("no noble 4")
+			goto notnoble
+		end
+		
+		imgui.Text("Is noble")
+		
+		::notnoble::
+	end
 end
 
 function Inspector:render()
@@ -37,6 +90,8 @@ function Inspector:render()
 		local first_name = dfhack.df2utf(language_name.first_name)
 		
 		imgui.Text("Race: " .. race_name .. " : Name:" .. first_name) 
+		
+		noble_position(unit)
 		
 		::continue::
 	end
