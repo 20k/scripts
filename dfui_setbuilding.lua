b24ea84eabfecd206cf6828d4c03070be7a0d0b2
20k <icedshot@gmail.com>
2022-12-31 21:44:35 +0000

    add new task, existing jobs look at

--- dfui_setbuilding.lua
+++ dfui_setbuilding.lua
@@ -5,21 +5,29 @@ render = reqscript('dfui_render')
 
 imgui = dfhack.imgui
 
-selected_building_pos = {x=-1, y=-1, z=-1}
+function display_jobs(building, jobs)
+	for i,v in ipairs(jobs) do
+		imgui.Button(v.name .. "##" .. tostring(i))
+	end
+end
 
-function display_jobs(jobs)
-	for _,v in ipairs(jobs) do
-		imgui.Text(v.name)
+function display_existing_jobs(building)
+	local jobs = building.jobs
+	
+	for _,j in ipairs(jobs) do
+		imgui.Text(df.job_type.attrs[j.job_type].caption)
 	end
 end
 
 function render_setbuilding()
 	local mouse_world_pos = render.get_mouse_world_coordinates()
 	
+	local state = {screen="base"}
 	local selected_building = mouse_world_pos
 	
 	if render.get_menu_item() ~= nil then
-		selected_building = render.get_menu_item()
+		state = render.get_menu_item()
+		selected_building = state.pos
 	end
 	
 	local building = dfhack.buildings.findAtTile(selected_building)
@@ -27,34 +35,55 @@ function render_setbuilding()
 	if building == nil then
 		return
 	end
-	
-	local building_id = building.id
-	
+
 	local is_workshop = df.building_workshopst:is_instance(building)
-	
-	if is_workshop then
-		local type = df.building_type.Workshop
-		local subtype = building.type
-		
-		local jobs = workshops.getJobs(type, subtype, -1)
-		
-		display_jobs(jobs)
-	end
-	
 	local is_furnace = df.building_furnacest:is_instance(building)
 	
-	if is_furnace then
-		local type = df.building_type.Furnace
-		local subtype = building.type
-		--what is melt_remainder?
+	if is_workshop or is_furnace then
+		local name = df.workshop_type.attrs[building.type].name
 		
-		local jobs = workshops.getJobs(type, subtype, -1)
+		imgui.Text(name)
 		
-		display_jobs(jobs)
+		imgui.NewLine()
+	end
+	
+	if state.screen == "Add new task" then
+		if is_workshop then
+			local type = df.building_type.Workshop
+			local subtype = building.type
+			
+			local jobs = workshops.getJobs(type, subtype, -1)
+			
+			display_jobs(building, jobs)
+		end
+		
+		if is_furnace then
+			local type = df.building_type.Furnace
+			local subtype = building.type
+			--what is melt_remainder?
+			
+			local jobs = workshops.getJobs(type, subtype, -1)
+			
+			display_jobs(building, jobs)
+		end
+	end
+	
+	if state.screen == "base" then
+		display_existing_jobs(building)
+		
+		imgui.NewLine()
+		
+		local strings = {{key="a", text="Add new task"}}
+		
+		local next_state = render.render_table_impl(strings, "none")
+		
+		if next_state == "Add new task" then
+			render.set_menu_item({screen=next_state, pos=selected_building})
+		end
 	end
 	
 	if imgui.IsMouseClicked(0) and not imgui.WantCaptureMouse() then
-		render.set_menu_item(mouse_world_pos)
+		render.set_menu_item({screen="base", pos=mouse_world_pos})
 	end
 	
 	if imgui.IsMouseClicked(1) and imgui.WantCaptureMouse() then
