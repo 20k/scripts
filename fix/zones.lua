846a2d266b19465449fe074beebb0f784f14c879
20k <icedshot@gmail.com>
2023-01-19 22:16:23 +0000

    Simplify, nobody look but I mistyped canMakeRoom as canBeRoom

--- fix/zones.lua
+++ fix/zones.lua
@@ -4,54 +4,42 @@ local function id_sorter(a, b)
 	if a.id == b.id then
 		return 0
 	end
-	
+
 	if a.id > b.id then
 		return 1
 	end
-	
+
 	return -1
 end
 
 function building_into_zone_unidir(bld, zone)
-	for k,v in ipairs(zone.contained_buildings) do
-		if v == bld then
-			return
-		end
-	end
-	
 	zone.contained_buildings:insert('#', bld)
-	
+
 	utils.sort_vector(zone.contained_buildings, nil, id_sorter)
 end
 
 function zone_into_building_unidir(bld, zone)
-	for k,v in ipairs(bld.relations) do
-		if v == zone then
-			return
-		end
-	end
-
 	bld.relations:insert('#', zone)
 
 	utils.sort_vector(bld.relations, nil, id_sorter)
 end
 
 local function add_to_zones(bld)
-	if not bld:canBeRoom() then
+	if not bld:canMakeRoom() then
 		return
 	end
-	
+
 	local pos = xyz2pos(bld.centerx, bld.centery, bld.z)
-	
+
 	local zones = dfhack.buildings.findCivzonesAt(pos)
-	
+
 	for _, zone in ipairs(zones) do
 		zone_into_building_unidir(bld, zone)
 		building_into_zone_unidir(bld, zone)
 	end
 end
 
-for k,zone in ipairs(df.global.world.buildings.other.ACTIVITY_ZONE) do
+for k, zone in ipairs(df.global.world.buildings.other.ACTIVITY_ZONE) do
 	zone.contained_buildings:resize(0)
 end
 
