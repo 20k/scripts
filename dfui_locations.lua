bff9c6857562850c720aa7bef27127f4a0eea9f2
20k <icedshot@gmail.com>
2023-01-24 03:02:11 +0000

    fixes

--- dfui_locations.lua
+++ dfui_locations.lua
@@ -76,7 +76,26 @@ function clean_invalid_ids(location)
     end
 end
 
+function remove_zone_from_all_locations(zone)
+    local locations = get_locations()
+
+    for k,location in ipairs(locations) do
+        local buildings = location:getContents().building_ids
+
+        for i=#buildings-1,0,-1 do
+            if buildings[i] == zone.id then
+                buildings:erase(i)
+            end
+        end
+    end
+
+    zone.location_id = -1
+    zone.site_id = -1
+end
+
 function on_assign_location(zone, location)
+    remove_zone_from_all_locations(zone)
+
     if location ~= nil then
         clean_invalid_ids(location)
 
@@ -93,9 +112,6 @@ function on_assign_location(zone, location)
 
         buildings:insert('#', zone.id)
         utils.sort_vector(buildings, nil, id_sorter)
-    else
-        zone.location_id = -1
-        zone.site_id = -1
     end
 end
 
@@ -122,10 +138,36 @@ end
 function render_locations()
     render.set_can_window_pop(true)
 
+    --[[local type_to_name = {
+        df.abstract_building_type.TEMPLE="Temple",
+        df.abstract_building_type.TOMB="Tomb",
+        df.abstract_building_type.INN_TAVERN="Inn/Tavern",
+        df.abstract_building_type.GUILDHALL="Guildhall",
+        df.abstract_building_type.HOSPITAL="Hospital"
+    }]]--
+
+    local name_to_type = {
+        ["Temple"] = df.abstract_building_type.TEMPLE,
+        ["Tomb"] = df.abstract_building_type.TOMB,
+        ["Inn/Tavern"] = df.abstract_building_type.INN_TAVERN,
+        ["Guildhall"] = df.abstract_building_type.GUILDHALL,
+        ["Hospital"] = df.abstract_building_type.HOSPITAL
+    }
+
+    local type_to_name = {}
+
+    for i,j in pairs(name_to_type) do
+        type_to_name[j] = i
+    end
+
     local locations = get_locations()
 
     for k,location in ipairs(locations) do
         imgui.Text(get_location_name(location))
+
+        imgui.SameLine()
+
+        imgui.Text(type_to_name[location:getType()])
 
         local contents = location:getContents()
 
